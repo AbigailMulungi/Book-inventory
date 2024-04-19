bb9984b5b054c24d3c1afc218758972bd3c1c5ee
AbigailMulungi <muluabie5@gmail.com>
2024-04-19 19:40:11 +0300

    Project setup

--- examcs/src/test/java/com/abigail/examcs/ExamcsApplicationTests.java
+++ examcs/src/test/java/com/abigail/examcs/ExamcsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.abigail.practicleexam;
+package com.abigail.examcs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PracticleexamApplicationTests {
+class ExamcsApplicationTests {
 
 	@Test
 	void contextLoads() {
