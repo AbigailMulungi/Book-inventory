d40712afc944f1017ad8813cd29a14c7a0e03f77
AbigailMulungi <muluabie5@gmail.com>
2024-04-19 19:01:05 +0300

    project initialization

---
+++ practicleexam/src/test/java/com/abigail/practicleexam/PracticleexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abigail.practicleexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticleexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
